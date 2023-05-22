22118ed2fbba6f5a143c78e55fbfd6e8f1dc14f7
wenbo <wenbo.wan@chimeinc.com>
2023-05-22 14:14:14 +0800

    修改了ci流程

--- selenium_handler/runner.py
+++ selenium_handler/runner.py
@@ -140,6 +140,7 @@ def run_case(code, options, model='chrome'):
             exec(code)
             break
         except Exception as e:
+            logging.info(e)
             append_log(
                 "ignore-error",
                 f"retry {i + 1} end"
