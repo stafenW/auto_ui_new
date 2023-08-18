9e78457cbca27afe26e67dea584de1bd0d11c34b
wilburw <41094099+stafenW@users.noreply.github.com>
2023-08-18 16:45:59 +0800

    Update check_alert.py

--- alert/check_alert.py
+++ alert/check_alert.py
@@ -13,7 +13,8 @@ def check_result(model='chrome'):
 
 
 def alert_qa():
-    url = 'http://alert.d.chime.me/api/notify'
+    # 自己的通知地址
+    url = 'xxx'
     chrome_check_count, chrome_error_time, chrome_error_cases = check_result('chrome')
     safari_check_count, safari_error_time, safari_error_cases = check_result('safari')
 
